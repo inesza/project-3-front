8144b2f1cebca98547e5e128c6a230c4b056caa3
Ines Zallouz <ines.zallouz@gmail.com>
2022-11-30 13:00:06 +0100

    Fix Routerdom deprecated alert

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import UserProvider from "./auth/UserProvider";
 import "./styles/reset.css";
 import "./styles/global.css";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<UserProvider>
-				<App />
-			</UserProvider>
-		</Router>
-	</React.StrictMode>,
-	document.getElementById("root")
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </Router>
+  </React.StrictMode>
 );
