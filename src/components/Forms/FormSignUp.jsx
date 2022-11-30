23f17bc74d7a76cced20492b4473e45b8d77a901
Ines Zallouz <ines.zallouz@gmail.com>
2022-11-30 12:47:25 +0100

    Sign up form + react update

--- src/components/Forms/FormSignUp.jsx
+++ src/components/Forms/FormSignUp.jsx
@@ -1,57 +1,123 @@
-import useForm from "../../hooks/useForm"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import apiHandler from "../../api/apiHandler"
+// import useForm from "../../hooks/useForm";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import apiHandler from "../../api/apiHandler";
+import FormSignUpStep1 from "./FormSignUp/FormSignUpStep1";
+import FormSignUpStep2 from "./FormSignUp/FormSignUpStep2";
+import { motion } from "framer-motion";
 
 const FormSignUp = () => {
-	const [values, handleChange] = useForm({ name: "", email: "", password: "" })
-	const [error, setError] = useState(null)
-	const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [x, setX] = useState(0);
+  const [page, setPage] = useState(0);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    gender: "",
+    birth: "",
+  });
+  const stepsList = [
+    <FormSignUpStep1
+      page={page}
+      setPage={setPage}
+      formData={formData}
+      setFormData={setFormData}
+      x={x}
+      setX={setX}
+    />,
+    <FormSignUpStep2
+      page={page}
+      setPage={setPage}
+      formData={formData}
+      setFormData={setFormData}
+      x={x}
+      setX={setX}
+    />,
+  ];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    apiHandler
+      .signup(formData)
+      .then(() => {
+        alert("Created");
+        // navigate("/signin");
+      })
+      .catch((error) => {
+        setError(error.response.data);
+      });
+  };
+  return (
+    <section>
+      <div
+        className="progress-bar"
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "20px",
+          justifyContent: page === 0 ? "flex-start" : "flex-end",
+        }}
+      >
+        <div style={{ width: "50%", background: "red" }}></div>
+      </div>
+      <div>Forms</div>
+      <form onClick={handleSubmit}>
+        {stepsList[page]}
+        <br />
+        {page === 1 && <button>Submit</button>}
+      </form>
+    </section>
+  );
+  // const [values, handleChange] = useForm({ name: "", email: "", password: "" })
+  // const [error, setError] = useState(null)
+  // const navigate = useNavigate()
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		apiHandler
-			.signup(values)
-			.then(() => {
-				navigate("/signin")
-			})
-			.catch((error) => {
-				setError(error.response.data)
-			})
-	}
-	return (
-		<>
-			{error && <h3 className="error">{error.message}</h3>}
-			<form onSubmit={handleSubmit}>
-				<h2>Signup</h2>
-				<label htmlFor="name">Name</label>
-				<input
-					onChange={handleChange}
-					value={values.name}
-					type="text"
-					id="name"
-					name="name"
-				/>
-				<label htmlFor="email">Email</label>
-				<input
-					onChange={handleChange}
-					value={values.email}
-					type="email"
-					id="email"
-					name="email"
-				/>
-				<label htmlFor="password">Password</label>
-				<input
-					onChange={handleChange}
-					value={values.password}
-					type="password"
-					id="password"
-					name="password"
-				/>
-				<button>Submit</button>
-			</form>
-		</>
-	)
-}
+  // const handleSubmit = (e) => {
+  // 	e.preventDefault()
+  // 	apiHandler
+  // 		.signup(values)
+  // 		.then(() => {
+  // 			navigate("/signin")
+  // 		})
+  // 		.catch((error) => {
+  // 			setError(error.response.data)
+  // 		})
+  // }
+  // return (
+  // 	<>
+  // 		{error && <h3 className="error">{error.message}</h3>}
+  // 		<form onSubmit={handleSubmit}>
+  // 			<h2>Signup</h2>
+  // 			<label htmlFor="name">Name</label>
+  // 			<input
+  // 				onChange={handleChange}
+  // 				value={values.name}
+  // 				type="text"
+  // 				id="name"
+  // 				name="name"
+  // 			/>
+  // 			<label htmlFor="email">Email</label>
+  // 			<input
+  // 				onChange={handleChange}
+  // 				value={values.email}
+  // 				type="email"
+  // 				id="email"
+  // 				name="email"
+  // 			/>
+  // 			<label htmlFor="password">Password</label>
+  // 			<input
+  // 				onChange={handleChange}
+  // 				value={values.password}
+  // 				type="password"
+  // 				id="password"
+  // 				name="password"
+  // 			/>
+  // 			<button>Submit</button>
+  // 		</form>
+  // 	</>
+  // )
+};
 
-export default FormSignUp
+export default FormSignUp;
---
+++ src/components/Forms/FormSignUp/FormSignUpStep2.jsx
@@ -0,0 +1,40 @@
+import { motion } from "framer-motion";
+
+const FormSignUpStep2 = ({ page, setPage, formData, setFormData, x, setX }) => {
+  return (
+    <motion.div
+      initial={{ x: x }}
+      transition={{ duration: 1 }}
+      animate={{ x: 0 }}
+    >
+      <div>Tell us more</div>
+      <select
+        name="gender"
+        id="gender"
+        value={formData.value}
+        onChange={(e) => setFormData({ ...formData, gender: e.target.value })}
+      >
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="nb-gf">Non binary / Gender fluid</option>
+        <option value="other">Other</option>
+      </select>
+      <input
+        type="date"
+        placeholder="Birth"
+        value={formData.birth}
+        onChange={(e) => setFormData({ ...formData, birth: e.target.value })}
+      />
+      <button
+        onClick={() => {
+          setPage(page - 1);
+          setX(-1000);
+        }}
+      >
+        Previous
+      </button>
+    </motion.div>
+  );
+};
+
+export default FormSignUpStep2;
