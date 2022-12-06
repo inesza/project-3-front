7aa97ede343de10ada0564fcb85a14561cd7b36d
Ines Zallouz <ines.zallouz@gmail.com>
2022-12-06 10:02:11 +0100

    handle profile deletion

--- src/components/Forms/FormSignUp.jsx
+++ src/components/Forms/FormSignUp.jsx
@@ -6,12 +6,15 @@ import service from "../../api/apiHandler";
 import axios from "axios";
 import FormSignUpStep1 from "./FormSignUp/FormSignUpStep1";
 import FormSignUpStep2 from "./FormSignUp/FormSignUpStep2";
+import ModalConfirmDelete from "../ModalConfirmDelete";
+import useModal from "../../hooks/useModal";
 
 const FormSignUp = ({ edit }) => {
   const navigate = useNavigate();
   const [error, setError] = useState(null);
   const [x, setX] = useState(0);
   const [page, setPage] = useState(0);
+  const { isShowing, toggleModal } = useModal();
   const [formData, setFormData] = useState({
     username: "",
     email: "",
@@ -43,7 +46,6 @@ const FormSignUp = ({ edit }) => {
       service
         .patch("/api/auth/edit", formData)
         .then((res) => {
-          console.log(res.data);
           navigate("/profile");
           navigate(0);
         })
@@ -52,13 +54,28 @@ const FormSignUp = ({ edit }) => {
       apiHandler
         .signup(formData)
         .then(() => {
-          alert("Created");
           navigate("/signin");
         })
         .catch((error) => {
           setError(error.response);
         });
     }
+  };
+  const handleDelete = () => {
+    service
+      .delete(`/api/auth/delete`)
+      .then(
+        (document.querySelector(".modal-header h3").textContent =
+          "Profile deleted")
+      )
+      .then(
+        setTimeout(() => {
+          navigate("/");
+        }, 1000)
+      )
+      .catch((e) => {
+        console.error(e.message);
+      });
   };
   return (
     <section>
@@ -80,6 +97,17 @@ const FormSignUp = ({ edit }) => {
           {page === 1 && <button>Submit</button>}
         </form>
       </section>
+      {edit && (
+        <>
+          <button onClick={toggleModal}>Delete my profile</button>
+          <ModalConfirmDelete
+            isShowing={isShowing}
+            hide={toggleModal}
+            handleDelete={handleDelete}
+            modalTitle={"Delete my profile"}
+          />
+        </>
+      )}
     </section>
   );
 };
