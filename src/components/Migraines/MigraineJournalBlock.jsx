c14d66e8b24d9205919130556df1b8b88666519f
Ines Zallouz <ines.zallouz@gmail.com>
2022-12-07 17:00:57 +0100

    new potential fix for deploy issue

--- src/components/Migraines/MigraineJournalBlock.jsx
+++ src/components/Migraines/MigraineJournalBlock.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import service from "../../api/ApiHandler";
+import service from "./../../api/apiHandler";
 import MigraineSummaryCard from "./MigraineSummaryCard";
 
 const MigraineJournalBlock = ({ full }) => {
